f1796f425c2dabbe7ba975fa6ae854b68591cbc8
soma220 <81844085+soma220@users.noreply.github.com>
2025-03-20 09:27:46 +0000

    Added connection to db

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
-from sqlalchemy import create_engine, text, URL
+from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = 'mysql+pymysql://2VYR4UeraYPWA8c.root:kI02KHeOZKmyNYrm@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/test'
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={'ssl': {
                            'ssl_ca': '/etc/ssl/cert.pem'
                        }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print(result.all())
-  result_all = result.all()
-  print(type(result_all))
-  first_result = result_all[0]
-  first_result_dict = first_result.__dict__
-  print(type(first_result_dict))
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
