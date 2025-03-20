36f63678df506f4be2771457ec2bdcb8c1043aac
soma220 <81844085+soma220@users.noreply.github.com>
2025-03-20 10:58:25 +0000

    Dynamic pages for each jobs

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Jovian')
+  return render_template('home.html', jobs=jobs)
 
 @app.route('/api/jobs')
 def list_jobs():
